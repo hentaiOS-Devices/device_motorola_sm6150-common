604add1886529b4f5d9a8cce0730c8c50f01b9af
Lunarixus <Nathan@lunarixus.dev>
2021-01-30 23:02:01 +0000

    sm6150-common: Add RRO Overlay for SystemUIGoogle
    * Initial empty SystemUIGoogle overlay
    
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>
    Change-Id: I9138f5ce00197e78913ec26de183f48e9505d51e

--- common.mk
+++ common.mk
@@ -106,6 +106,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
 
+# SystemUIGoogle package
+PRODUCT_PACKAGES += \
+    SystemUIGoogleOverlayHentai
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
